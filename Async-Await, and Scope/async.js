27670bfa9414428ba02ae8bb3a6cd2b1cfbe8fa4
Programmeramin <developeramin2615@gmail.com>
2025-08-14 17:56:49 +0600

    some code added

--- Async-Await, and Scope/async.js
+++ Async-Await, and Scope/async.js
@@ -2,6 +2,33 @@ const fetchPromise = fetch(
   "bad-scheme://mdn.github.io/learning-area/javascript/apis/fetching-data/can-store/products.json",
 );
 
+
+fetchPromise.then((res) =>{
+    if(!res.ok){
+      throw new Error(`response error: ${res.status}`)
+    }
+
+    return res.json()
+}).then((res) =>{
+  console.log(`Product: ${res[0].name}`);
+  
+}).catch((error) =>{
+   console.log(`Could not get product ${error}`);
+   
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
 fetchPromise
   .then((response) => {
     if (!response.ok) {
